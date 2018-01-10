47a069e5c8b461c3a24964df024b0a6f2cdf1936
calvin <butler2018@hotmail.com>
2018-01-10 13:45:53 +0800

    ver2

--- app/src/main/java/com/example/student/a18011003/MainActivity.java
+++ app/src/main/java/com/example/student/a18011003/MainActivity.java
@@ -14,20 +14,24 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
     Handler handler = new Handler(){
         @Override
         public void handleMessage(Message msg)
         {
             super.handleMessage(msg);
             Log.d("MSG","取得一個 Message");
+            Bundle b= msg.getData();
+            String str = b.getString("data");
+            Log.d("MSG","data" + str);
+
         }
     };
     public void click1(View v)
     {
         Message msg = new Message();
-        handler.sendMessage(msg);
+        Bundle b = new Bundle();
+        b.putString("data","Mydata");
+        msg.setData(b);
+        handler.sendMessageDelayed(msg,3000);
     }
-
-
 }
